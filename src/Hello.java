ace41469b459d95ae1db4cd18e721794de83bbf0
alebul <master8423@gmail.com>
2023-04-06 22:42:33 +0300

    lesson 20 done

--- src/Hello.java
+++ src/Hello.java
@@ -2,18 +2,51 @@ import java.util.Scanner;
 
 public class Hello {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    Person ps1 = new Person("Alex", 20);
+    Person ps2 = new Person("Bob", 30);
+    Person ps3 = new Person("John", 40);
+    Person ps4 = new Person();
 
-    int i = sc.nextInt();
+    System.out.println(ps1.getName());
+    System.out.println(ps1.getAge());
+    System.out.println(ps2.getName());
+    System.out.println(ps2.getAge());
+    System.out.println(ps3.getName());
+    System.out.println(ps3.getAge());
 
 
-    switch (i) {
-      case 10:
-        System.out.println(i);
-        break;
-      case 11:
-        System.out.println(i);
-        break;
-    }
   }
 }
+
+
+class Person {
+  private String name;
+  private int age;
+
+  public Person(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public Person() {
+    System.out.println("Empty person");
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+}
+
+
