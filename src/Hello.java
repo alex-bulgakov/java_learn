90f35a627bc83bf4e0f5239cb82a8a5eedce8506
alebul <master8423@gmail.com>
2023-04-06 22:17:49 +0300

    lesson 10 done

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Hello {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int i = sc.nextInt();
+
+
+    switch (i) {
+      case 10:
+        System.out.println(i);
+        break;
+      case 11:
+        System.out.println(i);
+        break;
+    }
+  }
+}
